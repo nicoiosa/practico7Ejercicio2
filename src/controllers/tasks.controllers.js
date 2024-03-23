936e7c220d22d246bf0b9f941727676c179e6ae1
nicoIosa <nicolasiosa@hotmail.com>
2024-03-23 11:32:15 -0300

    created controllers and added to routes

---
+++ src/controllers/tasks.controllers.js
@@ -0,0 +1,44 @@
+import Task from "../database/models/task";
+
+export const listTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({
+      message: "Could not find the list of tasks",
+    });
+  }
+};
+export const createTask = async (req, res) => {
+  try {
+    const newTask = new Task(req.body);
+    await newTask.save();
+    res.status(201).json({
+      message: "The task was created",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      message: "The task could not be uploaded",
+    });
+  }
+};
+export const deleteTask = async (req, res) => {
+  try {
+    const searchedTask = await Task.findById(req.params.id);
+    if (!searchedTask) {
+      return res.status(404).json({
+        message: "Could not find the task",
+      });
+    }
+    await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "The task was deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "An error ocurred trying to delete the task",
+    });
+  }
+};
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import {
+  createTask,
+  deleteTask,
+  listTasks,
+} from "../controllers/tasks.controllers";
 
 const router = Router();
 
-router.route("/tasks").get().post();
-router.route("/tasks/:id").delete();
+router.route("/tasks").get(listTasks).post(createTask);
+router.route("/tasks/:id").delete(deleteTask);
 
 export default router;
